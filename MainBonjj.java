b6b17d3da2303179409a335a08681dd298409330
Ankita Thakur <91601431+ankita-65825@users.noreply.github.com>
2022-10-13 11:47:02 +0530

    Add files via upload

---
+++ MainBonjj.java
@@ -0,0 +1,19 @@
+package imagecontroller;
+
+public class MainBonjj {
+	// Begin Class
+		
+		public MainBonjj () {
+			login.firstWind();
+		}
+		
+		public static void main (String[] args) {
+		// Begin main()
+			
+			new MainBonjj();
+			
+		// End main()
+		}
+		
+	// End Class
+	}
